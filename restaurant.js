6ab16527748965074a0fa033d669b8b7a9ac80da
KshitizSharma11 <84636204+KshitizSharma11@users.noreply.github.com>
2021-07-01 09:14:56 +0530

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,7 +1,7 @@
 var images = new Array(
     'https://images.squarespace-cdn.com/content/v1/5755bccb8259b515333df0e1/1466120130465-9BNATBEVMW9VCWH2KMA2/Stocksy_comp_461841-color.jpg',
-    'https://images.squarespace-cdn.com/content/v1/5755bccb8259b515333df0e1/1466120517882-GMTUN8QUTQPXD3SSUKAE/Stocksy_comp_486556.jpg',
-    'https://images.squarespace-cdn.com/content/v1/5755bccb8259b515333df0e1/1466119893918-X0SATSKR7JA3GP361K3R/Stocksy_comp_821143-crop.jpg',
+    'https://images.squarespace-cdn.com/content/v1/5755bccb8259b515333df0e1/1475088749256-645IUUCVAMPQYIQSAPVK/Stocksy_comp_865498.jpg',
+    'https://images.squarespace-cdn.com/content/v1/5755bccb8259b515333df0e1/1475344319269-I7F3K5K5OY1KWTZG7KYA/65_AP_Gougeres_v2-127.jpg',
     'https://images.squarespace-cdn.com/content/v1/5755bccb8259b515333df0e1/1466111941034-GIIKB6HB6ADZB2O0D7HW/Stocksy_comp_461838.jpg',
   );
   var slider = setInterval(function() {
